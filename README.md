64138c48fe6ed665ba542748e104461fb2067439
blackgoldi <goldik.black@yandex.ru>
2025-02-18 22:52:23 +0300

    Пятый шаг

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/blackgoldi/fullstack-javascript-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/blackgoldi/fullstack-javascript-project-44/actions)
 
 <a href="https://codeclimate.com/github/blackgoldi/fullstack-javascript-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/a709036dfc6a3e9a7e61/maintainability" /></a>
+<a href="https://asciinema.org/a/704178" target="_blank"><img src="https://asciinema.org/a/704178.svg" /></a>
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,20 @@
 import readlineSync from 'readline-sync';
 import name from '../src/cli.js';
 
-const isEven = (num) => num % 2 == 0;
+const isEven = (num) => num % 2 === 0;
 let counter = 0;
 do {
-	let rnd = Math.round(Math.random() * 10);
-	console.log(`Question: ${rnd}`);
-	let correctAnswer = isEven(rnd) ? 'yes' : 'no';
-	let answer = readlineSync.question('Your answer: ').toLowerCase();
-	if (correctAnswer == answer) {
-		counter++;
-		console.log('Correct');
-	}
-	else {
-		console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-		console.log(`Let's try again, ${name}!`);
-		break;
-	}
+  const rnd = Math.round(Math.random() * 10);
+  console.log(`Question: ${rnd}`);
+  const correctAnswer = isEven(rnd) ? 'yes' : 'no';
+  const answer = readlineSync.question('Your answer: ').toLowerCase();
+  if (correctAnswer === answer) {
+    counter += 1;
+    console.log('Correct');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
 } while (counter < 3);
-if (counter == 3) console.log(`Congratulations, ${name}!`);
+if (counter === 3) console.log(`Congratulations, ${name}!`);
